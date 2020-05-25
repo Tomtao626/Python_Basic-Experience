0082da534a54c26ef67a31e1f51ccc0290c20cbc
Tomtao626 <751825253@qq.com>
2020-05-25 23:38:30 +0800

    'url_parm'

--- python_django/chapter02/url_params_demo/book/views.py
+++ python_django/chapter02/url_params_demo/book/views.py
@@ -8,3 +8,12 @@ def book(request):
 def book_detail(request,book_id,category_id):
     text = "你获取的图书id是：%s,你的图书分类id是：%s" % (book_id,category_id)
     return HttpResponse(text)
+
+def author_detail(request):
+    author_id = request.GET.get['id']
+    text = '作者的id是:%s' % author_id
+    return HttpResponse(text)
+
+def publish_detail(request,publish_id):
+    text = '出版社的id是：%s'% publish_id
+    return HttpResponse(text)
